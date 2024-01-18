928bdc72a2e1bbee8f79d30110f980c79daa3b90
roll1226 <tomo_yasu_4724@icloud.com>
2024-01-18 12:22:47 +0900

    pinia study

---
+++ src/views/CounterView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore()
+const { count, doubleCount } = storeToRefs(store)
+const { increment } = store
+</script>
+
+<template>
+  <div>
+    <button @click="increment">+</button>
+    <p>
+      {{ count }}
+    </p>
+    <p>
+      {{ doubleCount }}
+    </p>
+  </div>
+</template>
